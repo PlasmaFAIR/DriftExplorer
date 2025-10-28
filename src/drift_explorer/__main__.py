4cb2c4b3a0318c79eb1ff3aceacf4057be153ae3
Peter Hill <peter.hill@york.ac.uk>
2025-10-28 17:56:23 +0000

    Handle Ctrl-C better

--- src/drift_explorer/__main__.py
+++ src/drift_explorer/__main__.py
@@ -1,10 +1,12 @@
 from PyQt6.QtWidgets import QApplication
 
 import sys
+import signal
 
 from .gui import DriftExplorer
 
 def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QApplication(sys.argv)
     window = DriftExplorer()
     window.show()
